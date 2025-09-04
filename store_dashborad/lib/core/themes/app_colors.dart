ee18040493a65d9adc6a8bdf592f42dc3bf8a76f
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-09-04 04:00:43 +0300

    add app colors

---
+++ store_dashborad/lib/core/themes/app_colors.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const transparent = Color(0x00000000);
+  static const red = Color(0xFFFF0000);
+  static const grayscale50 = Color(0xFFF2F3F3);
+  static const grayscale100 = Color(0xFFE4E6E7);
+  static const grayscale200 = Color(0xFFCACECE);
+  static const grayscale300 = Color(0xFFAFB5B6);
+  static const grayscale400 = Color(0xFF949D9E);
+  static const grayscale500 = Color(0xFF4E5556);
+  static const grayscale600 = Color(0xFF616A6B);
+  static const grayscale700 = Color(0xFF494F50);
+  static const grayscale800 = Color(0xFF313535);
+  static const grayscale900 = Color(0xFF181A1B);
+  static const grayscale950 = Color(0xFF0C0D0D);
+  static const green1500 = Color(0xFF1B5E37);
+  static const green150 = Color(0xFFEBF9F1);
+  static const green1950 = Color(0xFF06140C);
+  static const green1100 = Color(0xFFD7F4E3);
+  static const green1200 = Color(0xFFB0E8C7);
+  static const green1300 = Color(0xFF88DDAB);
+  static const green1400 = Color(0xFF60D290);
+  static const green1600 = Color(0xFF2D9F5D);
+  static const green1700 = Color(0xFF227745);
+  static const green1800 = Color(0xFF174F2E);
+  static const green1900 = Color(0xFF0B2817);
+  static const green50 = Color(0xFFEEFBEA);
+  static const green100 = Color(0xFFDDF7D4);
+  static const green200 = Color(0xFFBBEFA9);
+  static const green300 = Color(0xFF98E77E);
+  static const green400 = Color(0xFF76DF53);
+  static const green500 = Color(0xFF6CDC46);
+  static const green600 = Color(0xFF43AC20);
+  static const green700 = Color(0xFF328118);
+  static const green800 = Color(0xFF225610);
+  static const green900 = Color(0xFF112B08);
+  static const green950 = Color(0xFF081504);
+  static const orange50 = Color(0xFFFEF6E7);
+  static const orange100 = Color(0xFFFDECCE);
+  static const orange200 = Color(0xFFFADA9E);
+  static const orange300 = Color(0xFFF8C76D);
+  static const orange400 = Color(0xFFF5B53D);
+  static const orange500 = Color(0xFFF4A91F);
+  static const orange600 = Color(0xFFC2820A);
+  static const orange700 = Color(0xFF926107);
+  static const orange800 = Color(0xFF614105);
+  static const orange900 = Color(0xFF312002);
+  static const orange950 = Color(0xFF181001);
+
+  // custom colors
+  static const inputBackgroundColor = Color(0xFFF9FAFA);
+  static const inputBorderColor = Color(0xFFE6E9EA);
+}
